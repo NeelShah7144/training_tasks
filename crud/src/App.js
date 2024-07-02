121ba36f32e860f0cd73274a628668036cb68d64
NeelShah7144 <neel.web.moweb@gmail.com>
2024-07-02 09:56:24 +0530

    feature: reacthooktask

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,11 +1,16 @@
-
-import FormMain from  './components/FormMain';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FormMain from './components/FormMain';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <>
-      <FormMain />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FormMain />} />
+        <Route path="/HomePage" element={<HomePage />} />
+      </Routes>
+    </Router> 
   );
 }
 
--- crud/src/components/FormMain.js
+++ crud/src/components/FormMain.js
@@ -1,37 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function FormMain() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
+  const emailRef = useRef(null);
+  const nameRef = useRef(null);
   const [storeValue, setStoreValue] = useState([]);
   const [id, setId] = useState(0);
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-  
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
+    const email = emailRef.current.vdalue;
+    const name = nameRef.current.value;
     setStoreValue([...storeValue, { id: id, email: email, name: name }]);
     setId(id + 1);
-    setEmail("");
-    setName("");
+    emailRef.current.value = "";
+    nameRef.current.value = "";
+    setIsSubmitted(true);
+    navigate("/HomePage"); 
   };
 
+  useEffect(() => {
+    if (isSubmitted) {
+      alert("Submitted");
+      setIsSubmitted(false);
+    }
+  }, [isSubmitted]);
+
   return (
-    <div>
+    <div className='bg-dark text-light col-md-6'>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">Email address</label>
           <input
             type="email"
-            className="form-control"
-            value={email}
-            onChange={handleEmailChange}
+            className="form-control bg-dark text-light"
+            ref={emailRef}
             id="email"
             aria-describedby="emailHelp"
           />
@@ -41,9 +46,8 @@ export default function FormMain() {
           <label htmlFor="name" className="form-label">Name</label>
           <input
             type="text"
-            className="form-control"
-            value={name}
-            onChange={handleNameChange}
+            className="form-control bg-dark text-light"
+            ref={nameRef}
             id="name"
             aria-describedby="nameHelp"
           />
@@ -53,10 +57,10 @@ export default function FormMain() {
       </form>
       <div>
         <h2>Your submitted details are:</h2>
-        <ul>
+        <ul>  
           {storeValue.map(item => (
             <li key={item.id}>
-              Email: {item.email}, Name: {item.name}
+              Id: {item.id}, Email: {item.email}, Name: {item.name}
             </li>
           ))}
         </ul>
